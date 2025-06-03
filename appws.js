b29ff19083d9d3e53fcfb8c6f6034cc928853b47
xueqino1 <xueqiqi@gmail.com>
2025-06-03 21:18:15 +0800

    add ndwsvless
    add init files

---
+++ appws.js
@@ -0,0 +1,73 @@
+const os = require('os');
+const http = require('http');
+const { Buffer } = require('buffer');
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+const net = require('net');
+const { exec, execSync } = require('child_process');
+const { WebSocket, createWebSocketStream } = require('ws');
+const logcb = (...args) => console.log.bind(this, ...args);
+const errcb = (...args) => console.error.bind(this, ...args);
+const UUID = process.env.UUID || 'b28f60af-d0b9-4ddf-baaa-7e49c93c380b';
+const uuid = UUID.replace(/-/g, "");
+const DOMAIN = process.env.DOMAIN || '';  //项目域名或已反代的域名，不带前缀，建议填已反代的域名
+const NAME = process.env.NAME || 'NDWS';
+const port = process.env.PORT || 3000;
+
+// 创建HTTP路由
+const httpServer = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World\n');
+  } else if (req.url === '/sub') {
+    const vlessURL = `vless://${UUID}@ip.sb:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${NAME}`;
+    
+    const base64Content = Buffer.from(vlessURL).toString('base64');
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(base64Content + '\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+httpServer.listen(port, () => {
+  console.log(`HTTP Server is running on port ${port}`);
+});
+
+
+const wss = new WebSocket.Server({ server: httpServer });
+wss.on('connection', ws => {
+  console.log("WebSocket 连接成功");
+  ws.on('message', msg => {
+    if (msg.length < 18) {
+      console.error("数据长度无效");
+      return;
+    }
+    try {
+      const [VERSION] = msg;
+      const id = msg.slice(1, 17);
+      if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) {
+        console.error("UUID 验证失败");
+        return;
+      }
+      let i = msg.slice(17, 18).readUInt8() + 19;
+      const port = msg.slice(i, i += 2).readUInt16BE(0);
+      const ATYP = msg.slice(i, i += 1).readUInt8();
+      const host = ATYP === 1 ? msg.slice(i, i += 4).join('.') :
+        (ATYP === 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) :
+          (ATYP === 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));
+      console.log('连接到:', host, port);
+      ws.send(new Uint8Array([VERSION, 0]));
+      const duplex = createWebSocketStream(ws);
+      net.connect({ host, port }, function () {
+        this.write(msg.slice(i));
+        duplex.on('error', err => console.error("E1:", err.message)).pipe(this).on('error', err => console.error("E2:", err.message)).pipe(duplex);
+      }).on('error', err => console.error("连接错误:", err.message));
+    } catch (err) {
+      console.error("处理消息时出错:", err.message);
+    }
+  }).on('error', err => console.error("WebSocket 错误:", err.message));
+});
